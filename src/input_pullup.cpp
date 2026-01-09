324ec612e0b0b9bb225f460c0f01726af4d9027b
ghost-mann <iamaob8@protonmail.com>
2026-01-09 15:25:54 +0300

    feat: added logic for loop in input_pullup.cpp observing state changes via serial monitor

--- src/input_pullup.cpp
+++ src/input_pullup.cpp
@@ -1,13 +1,17 @@
 #include <Arduino.h>
 #include "state.h"
 
+int ButtonState = 0;
 int pullup_buttonPin = 2;
 
 void pullup_init() {
     Serial.begin(9600);
-    pinMode(pullup_buttonPin,INPUT);
+    pinMode(2,INPUT_PULLUP);
 }
 
 void pullup_loop() {
-
+    int ButtonState = digitalRead(pullup_buttonPin);
+    Serial.println(ButtonState);
+    delay(1);
+    
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
+#include "state.h"
 
 
 void setup() {
-
+  pullup_init();
 }
 
 void loop() {
-
+  pullup_loop();
 }
 
