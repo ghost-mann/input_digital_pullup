6be446cc44bba0621849110425e90d327aaf5bae
ghost-mann <iamaob8@protonmail.com>
2026-01-12 15:01:53 +0300

    feat: added detection state for the button flipping modes between HIGH and LOW in state_change.cpp

--- src/state_change.cpp
+++ src/state_change.cpp
@@ -2,6 +2,7 @@
 #include "state.h"
 
 const int buttonPin = 2;
+int detectionState = 0;
 int buttonState = 0;
 int lastButtonState = 0;
 
@@ -9,9 +10,17 @@ void state_init() {
     Serial.begin(9600);
     pinMode(buttonPin,INPUT);
 
-
 }
 
 void state_loop() {
+    buttonState = digitalRead(buttonPin);
+    if (buttonState != lastButtonState) {
+         if (buttonState == HIGH) {
+            detectionState=(detectionState+1)%2;
+            Serial.println(detectionState);
+         }
+         delay(50);
+    }
+    lastButtonState = buttonState;
 
 }
